057018938862de6e0326e57b3ba69721a7923c0d
Bazelsneeze <pcleary2006@icloud.com>
2024-04-20 16:06:36 -0700

    More work on relay

--- Assets/Scripts/TestRelay.cs
+++ Assets/Scripts/TestRelay.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private async void Start()
     {
-        
+      await UnityServices.InitializeAsync();
+      AuthenticationService.Instance.SignedIn += () =>
+      {
+       Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+      };   
+      await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        CreateRelay();  
     }
 
-    // Update is called once per frame
-    void Update()
+    private async void CreateRelay()
     {
-        
+        try {
+           Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+
+           string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+           Debug.Log(joinCode);
+
+        } catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
     }
 }
