438fd1908bb0b2dbd2c05fa18cc18f7d88a09c84
TNTNINJA2 <yosemitebone@gmail.com>
2024-05-10 18:19:37 -0700

    Fixed bug in shader graph

--- Assets/Scripts/AttackHitboxDetector.cs
+++ Assets/Scripts/AttackHitboxDetector.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using static AttackHitboxDetector;
 
 public class AttackHitboxDetector : MonoBehaviour
 {
     PlayerController playerController;
     [SerializeField] CircleCollider2D hitbox;
     [SerializeField] LayerMask targetLayers;
-    // Start is called before the first frame update
+    public List<int> nums = new List<int>();
+
+    [SerializeField]
+    public Hitbox[] hitboxes;
+
+    [SerializeField]
+    public Hitbox hitbox2;
+
+
+    public enum HitboxType
+    {
+        circle,
+        rectangle
+    }
+
     void Awake()
     {
         playerController = GetComponentInParent<PlayerController>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -44,5 +60,29 @@ public class AttackHitboxDetector : MonoBehaviour
             Gizmos.DrawSphere(hitbox.transform.position, hitbox.radius);
         }
 
+        foreach (Hitbox hitbox in hitboxes)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position + new Vector3(hitbox.position.x, hitbox.position.y, 0), hitbox.radius);
+        }
+
     }
 }
+
+[System.Serializable]
+public struct Hitbox
+{
+    [SerializeField]
+    public bool enabled;
+    [SerializeField]
+    public Vector2 position;
+    [SerializeField]
+    public HitboxType type;
+    [SerializeField]
+    public float radius;
+    [SerializeField]
+    public float width;
+    [SerializeField]
+    public float height;
+}
+
