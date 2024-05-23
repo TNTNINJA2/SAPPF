e6cb24193044e1a4efe6e642c7595e0865fcbcd4
TNTNINJA2 <yosemitebone@gmail.com>
2024-05-23 11:35:23 -0700

    Added custom images for handle caps of pos curve editor

--- Assets/Editor/AttackEditor.cs
+++ Assets/Editor/AttackEditor.cs
@@ -1,18 +1,28 @@
 
 using System.Drawing.Printing;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEditor.IMGUI.Controls;
+using UnityEditor.SearchService;
+using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 [CustomEditor(typeof(Attack))]
 public class AttackEditor : UnityEditor.Editor
 {
     public float time;
+    public static Color posKeyFrameColor = Color.red;
+    public static Color bezierControlColor = Color.Lerp(Color.red, Color.yellow, 0.5f);
+    public static Color selectedKeyFrameColor = Color.magenta;
+    public static bool shouldDrawBezierControls = true;
     public Attack attack;
     public PlayerController dummy;
+    public Vector2 previousMousePosition;
+    public int nearestHandle = -1;
 
     public override void OnInspectorGUI()
     {
@@ -20,6 +30,10 @@ public class AttackEditor : UnityEditor.Editor
 
 
         time = EditorGUILayout.Slider(time, 0, attack.GetTotalDuration());
+        posKeyFrameColor = EditorGUILayout.ColorField(posKeyFrameColor);
+        bezierControlColor = EditorGUILayout.ColorField(bezierControlColor);
+        selectedKeyFrameColor = EditorGUILayout.ColorField(selectedKeyFrameColor);
+        shouldDrawBezierControls = EditorGUILayout.Toggle("Draw Bezier Controls", shouldDrawBezierControls);
 
         dummy = GameObject.FindGameObjectWithTag("Dummy").GetComponent<PlayerController>();
 
@@ -36,12 +50,143 @@ public class AttackEditor : UnityEditor.Editor
 
     private void OnEnable()
     {
-        SceneView.duringSceneGui += OnSceneupdate;
+        //SceneView.duringSceneGui += OnSceneupdate;
+        SceneView.duringSceneGui += OnSceneGUI;
     }
 
     private void OnDisable()
     {
         SceneView.duringSceneGui -= OnSceneupdate;
+        SceneView.duringSceneGui -= OnSceneGUI;
+
+    }
+
+    private void OnSceneGUI(SceneView sceneView)
+    {
+        attack = (Attack)target;
+
+        int hoverIndex = -1;
+        hoverIndex = HandleUtility.nearestControl;
+
+
+        int index = 10;
+
+        DrawPosCurves();
+        DrawBezierControls();
+       
+
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+        {
+            nearestHandle = HandleUtility.nearestControl;
+            previousMousePosition = Event.current.mousePosition;
+
+            // Undo
+            Undo.RegisterCompleteObjectUndo(attack, "HandleTransform");
+            Undo.FlushUndoRecordObjects();
+        }
+        if (Event.current.type == EventType.MouseUp && Event.current.button == 0)
+        {
+            nearestHandle = -1;
+            previousMousePosition = Vector3.zero;
+        }
+        HandlePosKeyFrames(hoverIndex, ref index);
+        if (Event.current.type == EventType.MouseDrag && Event.current.button == 0)
+        {
+            previousMousePosition = Event.current.mousePosition;
+        }
+    }
+
+    private void DrawPosCurves()
+    {
+        Handles.color = Color.red;
+        for (int i = 0; i < attack.posKeyFrames.Count - 1; i++)
+        {
+            KeyFrame<PosKeyFrameData> posKeyFrame1 = attack.posKeyFrames[i];
+            KeyFrame<PosKeyFrameData> posKeyFrame2 = attack.posKeyFrames[i + 1];
+
+
+            Handles.DrawBezier(posKeyFrame1.data.pos, posKeyFrame2.data.pos,
+                posKeyFrame1.data.afterBezierControlPoint, posKeyFrame2.data.beforeBezierControlPoint,
+                Color.red, Texture2D.whiteTexture, 1);
+        }
+
+    }
+
+    private void DrawBezierControls()
+    {
+        if (shouldDrawBezierControls)
+        {
+            for (int i = 0; i < attack.posKeyFrames.Count; i++)
+            {
+                KeyFrame<PosKeyFrameData> posKeyFrame = attack.posKeyFrames[i];
+
+                Handles.color = bezierControlColor;
+                Handles.DrawLine(posKeyFrame.data.pos, posKeyFrame.data.beforeBezierControlPoint);
+                Handles.DrawLine(posKeyFrame.data.pos, posKeyFrame.data.afterBezierControlPoint);
+            }
+        }
+    }
+
+    private void HandlePosKeyFrames(int hoverIndex, ref int index)
+    {
+
+
+
+        for (int i = 0; i < attack.posKeyFrames.Count; i++)
+        {
+            index++;
+            KeyFrame<PosKeyFrameData> poskeyFrame = attack.posKeyFrames[i];
+            Handles.color = hoverIndex == index ? selectedKeyFrameColor : posKeyFrameColor;
+            CreateHandleCap(index, poskeyFrame.data.pos, Quaternion.LookRotation(Vector3.right, Vector3.up), 0.1f, Event.current.type);
+
+            if ((Event.current.type == EventType.MouseDrag && Event.current.button == 0) && nearestHandle == index)
+            {
+                Vector2 move = Camera.main.ScreenToWorldPoint(Event.current.mousePosition) - Camera.main.ScreenToWorldPoint(previousMousePosition);
+                Debug.Log(move);
+                KeyFrame<PosKeyFrameData> posKeyFrame = attack.posKeyFrames[i];
+                posKeyFrame.data.pos += new Vector2(move.x, -move.y);
+                posKeyFrame.data.beforeBezierControlPoint += new Vector2(move.x, -move.y);
+                posKeyFrame.data.afterBezierControlPoint += new Vector2(move.x, -move.y);
+
+            }
+        }
+
+        if (shouldDrawBezierControls)
+        {
+            for (int i = 0; i < attack.posKeyFrames.Count; i++)
+            {
+                index++;
+                DrawBezierControlsForPoint(index, hoverIndex, attack.posKeyFrames[i]);
+                index++;
+            }
+        }
+
+        if (Event.current.type == EventType.MouseDrag) SceneView.RepaintAll();
+
+
+    }
+
+    private void DrawBezierControlsForPoint(int index, int hoverIndex, KeyFrame<PosKeyFrameData> posKeyFrame)
+    {
+        Handles.color = hoverIndex == index ? selectedKeyFrameColor : bezierControlColor;
+        CreateHandleCap(index, posKeyFrame.data.beforeBezierControlPoint, Quaternion.LookRotation(Vector3.right, Vector3.up), 0.1f, Event.current.type);
+        CreateHandleCap(index + 1, posKeyFrame.data.afterBezierControlPoint, Quaternion.LookRotation(Vector3.right, Vector3.up), 0.1f, Event.current.type);
+
+        if ((Event.current.type == EventType.MouseDrag && Event.current.button == 0) && nearestHandle == index)
+        {
+            Vector2 move = Camera.main.ScreenToWorldPoint(Event.current.mousePosition) - Camera.main.ScreenToWorldPoint(previousMousePosition);
+            posKeyFrame.data.beforeBezierControlPoint += new Vector2(move.x, -move.y);
+        }
+        if ((Event.current.type == EventType.MouseDrag && Event.current.button == 0) && nearestHandle == index + 1)
+        {
+            Vector2 move = Camera.main.ScreenToWorldPoint(Event.current.mousePosition) - Camera.main.ScreenToWorldPoint(previousMousePosition);
+            posKeyFrame.data.afterBezierControlPoint += new Vector2(move.x, -move.y);
+        }
+    }
+
+    void CreateHandleCap(int id, Vector3 position, Quaternion rotation, float size, EventType eventType)
+    {
+        Handles.DotHandleCap(id, position, rotation, size, eventType);
     }
 
 
@@ -51,38 +196,11 @@ public class AttackEditor : UnityEditor.Editor
 
 
         // Set handle color
-        Handles.color = Color.red;
-        for (int i = 0; i < attack.posKeyFrames.Count - 1; i++)
-        {
-            KeyFrame<PosKeyFrameData> posKeyFrame1 = attack.posKeyFrames[i];
-            KeyFrame<PosKeyFrameData> posKeyFrame2 = attack.posKeyFrames[i + 1];
 
 
-            Handles.DrawBezier(posKeyFrame1.data.pos, posKeyFrame2.data.pos, 
-                posKeyFrame1.data.afterBezierControlPoint, posKeyFrame2.data.beforeBezierControlPoint, 
-                Color.red, Texture2D.whiteTexture, 1);
-        }
 
-        for (int i = 0; i < attack.posKeyFrames.Count; i++)
-        {
-            KeyFrame<PosKeyFrameData> posKeyFrame = attack.posKeyFrames[i];
-            
-            Vector2 newPos = Handles.DoPositionHandle(posKeyFrame.data.pos, Quaternion.identity);
 
-            Vector2 deltaPos = newPos - posKeyFrame.data.pos;
 
-            posKeyFrame.data.beforeBezierControlPoint += deltaPos;
-            posKeyFrame.data.afterBezierControlPoint += deltaPos;
-            posKeyFrame.data.pos = newPos; 
-
-            posKeyFrame.data.beforeBezierControlPoint = Handles.DoPositionHandle(posKeyFrame.data.beforeBezierControlPoint, Quaternion.identity);
-            posKeyFrame.data.afterBezierControlPoint = Handles.DoPositionHandle(posKeyFrame.data.afterBezierControlPoint, Quaternion.identity);
-
-            Handles.color = Color.green;
-            Handles.DrawLine(posKeyFrame.data.pos, posKeyFrame.data.beforeBezierControlPoint);
-            Handles.DrawLine(posKeyFrame.data.pos, posKeyFrame.data.afterBezierControlPoint);
-            
-        }
 
         for (int i = 0; i < attack.hitboxKeyFrames.Count; i++)
         {
